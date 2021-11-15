89a09cca93b0d952a7ce60e5af0416685eda42e4
SamVansp <56045691+SamVansp@users.noreply.github.com>
2021-11-15 13:49:38 -0600

    Cleanup and moving some code around

--- mac-map-js/script.js
+++ mac-map-js/script.js
@@ -5,61 +5,6 @@ const input = document.querySelector("Room");
 var roomNumber;
 var floorNumber; 
 
-var carnegie = [ // temporary fix until I can acces variables from other file 
-  { name: "04", type: "rect", cords: [[248,235],[187,87]]},
-  { name: "04E", type: "rect", cords: [[130,35],[198,79]]},
-  { name: "04F", type: "rect", cords: [[207,35],[243,76]]},
-  { name: "04D", type: "rect", cords: [[129,87],[180,133]]},
-  { name: "04C", type: "rect", cords: [[128,146],[181,195]]},
-  { name: "04H", type: "rect", cords: [[261,41],[293,82]]},
-  { name: "04A", type: "rect", cords: [[256,217],[335,278]]},
-  { name: "04G", type: "rect", cords: [[257,92],[357,130]]},
-  { name: "041", type: "rect", cords: [[300,41],[353,80]]},
-  { name: "03", type: "rect", cords: [[260,186],[291,208]]},
-  { name: "09", type: "rect", cords: [[343,226],[416,275]]},
-  { name: "01", type: "rect", cords: [[363,39],[395,91]]},
-  { name: "05", type: "rect", cords: [[402,40],[500,128]]},
-  { name: "06", type: "rect", cords: [[511,36],[627,179]]},
-  { name: "06B", type: "rect", cords: [[514,185],[628,281]]},
-  { name: "07B", type: "rect", cords: [[469,184],[503,273]]},
-  { name: "07A", type: "rect", cords: [[428,189],[460,270]]},
-  { name: "02", type: "rect", cords: [[359,100],[396,168]]},
-  { name: "04B", type: "poly", cords:[[130,203],[183,204],[182,239],[248,240],[248,282],[130,282]]},
-  { name: "304", type: "rect", cords:[[120,146],[235,269]]},
-  { name: "305", type: "rect", cords:[[121,24],[237,144]]},
-  { name: "306", type: "rect", cords:[[251,28],[303,102]]},
-  { name: "307", type: "rect", cords:[[279,103],[305,120]]},
-  { name: "303", type: "rect", cords:[[243,197],[319,262]]},
-  { name: "302", type: "rect", cords:[[254,173],[281,193]]},
-  { name: "308", type: "rect", cords:[[307,30],[345,119]]},
-  { name: "309", type: "rect", cords:[[347,27],[486,121]]},
-  { name: "309A", type: "poly", cords:[[64,57],[464,41],[485,42],[483,59]]},
-  { name: "310A", type: "rect", cords:[[489,24],[531,86]]},
-  { name: "310B", type: "rect", cords:[[535,24],[606,69]]},
-  { name: "310C", type: "rect", cords:[[555,75],[602,113]]},
-  { name: "310D", type: "rect", cords:[[556,123],[603,158]]},
-  { name: "310E", type: "rect", cords:[[555,172],[603,212]]},
-  { name: "310F", type: "rect", cords:[[537,215],[605,265]]},
-  { name: "310G", type: "rect", cords:[[490,219],[530,264]]},
-  { name: "310", type: "rect", cords:[[490,90],[549,211]]},
-  { name: "403", type: "rect", cords:[[233,167],[261,197]]},
-  { name: "404", type: "rect", cords: [[101,115],[219,205]]},
-  { name: "404A", type: "rect", cords:[[102,208],[216,268]]},
-  { name: "404B", type: "rect", cords:[[101,26],[221,111]]},
-  { name: "405", type: "rect", cords:[[234,71],[318,127]]},
-  { name: "407", type: "rect", cords:[[323,71],[363,126]]},
-  { name: "407A", type: "rect", cords:[[224,31],[464,66]]},
-  { name: "409", type: "rect", cords:[[369,71],[451,127]]},
-  { name: "410A", type: "poly", cords:[[471,27],[469,71],[456,69],[456,110],[584,111],[585,25]]},
-  { name: "410", type: "rect", cords:[[505,114],[584,163]]},
-  { name: "401", type: "rect", cords:[[463,112],[504,165]]},
-  { name: "412", type: "rect", cords:[[463,169],[488,202]]},
-  { name: "413", type: "rect", cords:[[380,167],[462,205]]},
-  { name: "413A", type: "rect", cords:[[386,231],[462,266]]},
-  { name: "411A", type: "rect", cords:[[470,234],[585,270]]},
-  { name: "411B", type: "rect", cords:[[466,207],[476,229]]},
-  { name: "411", type: "poly", cords:[[492,167],[490,203],[478,207],[478,228],[587,230],[582,168]]}
-  ];
 
   
 function changeImage1L() {
@@ -199,11 +144,12 @@ function render2() {
   ctx.drawImage(x, 0, 0);
 }
 
+// Function to find the room and its cordinates from the list of possible cordinates provided
 function findRoom(list, room) {
   let check = true;
-  for(let index = 0; index < list.length; ++index) { // right now assumes all rooms are rectangles, got to fix that. 
+  for(let index = 0; index < list.length; ++index) {  //loops through all the rooms and checks the name to see if it is correct
     if (list[index].name == room) {
-      drawRectangle(list[index]);
+      drawRectangle(list[index]); // right now assumes all rooms are rectangles, got to fix that.
       check = false;
     }
   }
@@ -223,12 +169,14 @@ function drawRectangle(room) {
   ctx.stroke();
 }
 
+
+// Function that is run by clicking the submit button
 function begin() { 
   let building = document.getElementById("build").value;
   let room = document.getElementById("textbox").value;
-  if(room.charAt(0) == floorNumber) {
+  if(room.charAt(0) == floorNumber) { // Checks to see if the room is on the same floor as the one being displayed. 
     if (building == "carn") {
-      findRoom(carnegie, room);
+      findRoom(carnegie, room); 
     }
     if (building == "library") {
       alert("We have not implemented this building yet");
@@ -245,6 +193,10 @@ function begin() {
 }
 else {
   alert("Room is not on this floor");
+}
+
+function cleanString(string) {
+  clean
 }
 
 
