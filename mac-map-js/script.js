ab56df5543777ca6747a0d3d96679e3dd15f0af4
SamVansp <56045691+SamVansp@users.noreply.github.com>
2021-11-15 09:35:55 -0600

    Debuggin + Tweaks
    Degugged what people added, for some reason rooms over 400 don't work??? Made the canvas clear betwene searches, added a check to see if the room is on the right floor and changed the visuals of highlight.

--- mac-map-js/script.js
+++ mac-map-js/script.js
@@ -7,6 +7,7 @@ olinRiceImages = ["https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2F
 carnegieImages = ["https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieBasement.png?v=1632508341217","https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieFirstFloor.png?v=1632508341147", "https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieSecondFloor.png?v=1632508341232",  "https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieThirdFloor.png?v=1632508341042", "https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieFourthFloor.png?v=1632508341282"];
 humanitiesImages = ["https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities1stFloor.png?v=1633620425247", "https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities2ndfloor.png?v=1633620429481", "https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities3rdfloor.png?v=1633620433471", "https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities4thfloor.png?v=1633620437421"]; 
 var roomNumber;
+var floorNumber; 
 
 var carnegie = [ // temporary fix until I can acces variables from other file 
   { name: "04", type: "rect", cords: [[248,235],[187,87]]},
@@ -27,7 +28,7 @@ var carnegie = [ // temporary fix until I can acces variables from other file
   { name: "07B", type: "rect", cords: [[469,184],[503,273]]},
   { name: "07A", type: "rect", cords: [[428,189],[460,270]]},
   { name: "02", type: "rect", cords: [[359,100],[396,168]]},
-  { name: "04B", type: "poly", cords: [[130,203],[183,204],[182,239],[248,240],[248,282],[130,282]]},
+  { name: "04B", type: "poly", cords:[[130,203],[183,204],[182,239],[248,240],[248,282],[130,282]]},
   { name: "304", type: "rect", cords:[[120,146],[235,269]]},
   { name: "305", type: "rect", cords:[[121,24],[237,144]]},
   { name: "306", type: "rect", cords:[[251,28],[303,102]]},
@@ -44,24 +45,24 @@ var carnegie = [ // temporary fix until I can acces variables from other file
   { name: "310E", type: "rect", cords:[[555,172],[603,212]]},
   { name: "310F", type: "rect", cords:[[537,215],[605,265]]},
   { name: "310G", type: "rect", cords:[[490,219],[530,264]]},
-  { name: "310", type: "rect", cords:[[490,90],[549,211]]},
-  { name: "403", type: "rect", coords=[[233,167],[261,197]]},
-  { name: "404", type: "rect", coords=[[101,115],[219,205]]},
-  { name: "404A", type: "rect", coords=[[102,208],[216,268]]},
-  { name: "404B", type: "rect", coords=[[101,26],[221,111]]},
-  { name: "405", type: "rect", coords=[[234,71],[318,127]]},
-  { name: "407", type: "rect", coords=[[323,71],[363,126]]},
-  { name: "407A", type: "rect", coords=[[224,31],[464,66]]},
-  { name: "409", type: "rect", coords=[[369,71],[451,127]]},
-  { name: "410A", type: "poly", coords=[[471,27],[469,71],[456,69],[456,110],[584,111],[585,25]],},
-  { name: "410", type: "rect", coords=[[505,114],[584,163]]},
-  { name: "401", type: "rect", coords=[[463,112],[504,165]]},
-  { name: "412", type: "rect", coords=[[463,169],[488,202]]},
-  { name: "413", type: "rect", coords=[[380,167],[462,205]]},
-  { name: "413A", type: "rect", coords=[[386,231],[462,266]]},
-  { name: "411A", type: "rect", coords=[[470,234],[585,270]]},
-  { name: "411B", type: "rect", coords=[[466,207],[476,229]]},
-  { name: "411", type: "ploy", coords=[[492,167],[490,203],[478,207],[478,228],[587,230],[582,168]]}
+  { name: "310", type: "rect", cords:[[490,90],[549,211]]}, // I don't understand why but for anything larger than 400 it does not work although 400 is fine.
+  { name: "403", type: "rect", coords:[[233,167],[261,197]]},
+  { name: "404", type: "rect", coords: [[101,115],[219,205]]},
+  { name: "404A", type: "rect", coords:[[102,208],[216,268]]},
+  { name: "404B", type: "rect", coords:[[101,26],[221,111]]},
+  { name: "405", type: "rect", coords:[[234,71],[318,127]]},
+  { name: "407", type: "rect", coords:[[323,71],[363,126]]},
+  { name: "407A", type: "rect", coords:[[224,31],[464,66]]},
+  { name: "409", type: "rect", coords:[[369,71],[451,127]]},
+  { name: "410A", type: "poly", coords:[[471,27],[469,71],[456,69],[456,110],[584,111],[585,25]]},
+  { name: "410", type: "rect", coords:[[505,114],[584,163]]},
+  { name: "401", type: "rect", coords:[[463,112],[504,165]]},
+  { name: "412", type: "rect", coords:[[463,169],[488,202]]},
+  { name: "413", type: "rect", coords:[[380,167],[462,205]]},
+  { name: "413A", type: "rect", coords:[[386,231],[462,266]]},
+  { name: "411A", type: "rect", coords:[[470,234],[585,270]]},
+  { name: "411B", type: "rect", coords:[[466,207],[476,229]]},
+  { name: "411", type: "ploy", coords:[[492,167],[490,203],[478,207],[478,228],[587,230],[582,168]]}
   ];
 
   
@@ -69,20 +70,21 @@ function changeImage1L() {
   var e1 = document.getElementById("build").value;
   if (e1 == "carn") {
       loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieFirstFloor.png?v=1632508341147");
+      floorNumber = 1;
   }
   if (e1 == "library") {
     // render;
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FLibraryFloor1.png?v=1632164257605");
-    return false;
+    floorNumber = 1;
   }
   
   if (e1 == "hum") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities1stFloor.png?v=1633620425247");
-    return false;
+    floorNumber = 1;
   }
   if (e1 == "orl") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FOlinRiceFirstFloor.png?v=1633620660885");
-    return false;
+    floorNumber = 1;
   }
 }
 
@@ -90,9 +92,11 @@ function changeImageLL() {
   var e1 = document.getElementById("build").value;
   if (e1 == "carn") {
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieBasement.png?v=1632508341217");
+    floorNumber = 0;
   }
   if (e1 == "library") {
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FLibraryLower.png?v=1632164280262");
+    floorNumber = 0;
   }
 }
 
@@ -100,16 +104,19 @@ function changeImage2L() {
   var e1 = document.getElementById("build").value;
   if (e1 == "carn") {
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieSecondFloor.png?v=1632508341232");
-    
+    floorNumber = 2;
   }
   if (e1 == "library") {
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FLibraryFloor2.png?v=1632164261836");
+    floorNumber = 2;
   }
   if (e1 == "hum") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities2ndfloor.png?v=1633620429481");
+    floorNumber = 2;
   }
   if (e1 == "orl") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FOlinRiceSecondFloor.png?v=1633620666941");
+    floorNumber = 2;
   }
 }
 
@@ -117,31 +124,40 @@ function changeImage3L() {
   var e1 = document.getElementById("build").value;
   if (e1 == "carn") {
   loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieThirdFloor.png?v=1632508341042");
+  floorNumber = 3;
   }
   if (e1 == "library") {
     loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FLibraryFloor3.png?v=1632164271313");
+    floorNumber = 3;
   }
   if (e1 == "hum") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities3rdfloor.png?v=1633620433471");
+    floorNumber = 3;
   }
   if (e1 == "orl") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FOlinRiceThirdFloor.png?v=1633620670465");
+    floorNumber = 3;
   }
 }
 
 function changeImage4L() {
   var e1 = document.getElementById("build").value;
+
   if (e1 == "carn") {
       loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FCarnegieFourthFloor.png?v=1632508341282");
+      floorNumber = 4;
   }
   if (e1 == "library") {
       loadImage("https://cdn.glitch.com/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FLibraryFloor4.png?v=1632164275904");
+      floorNumber = 4;
   }
   if (e1 == "hum") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FHumanities4thfloor.png?v=1633620437421");
+    floorNumber = 4;
   }
   if (e1 == "orl") {
     loadImage("https://cdn.glitch.me/dd9514b9-20ff-4bc5-b6b3-d7a1630812d3%2FOlinRiceFourthFloor.png?v=1633620674438");
+    floorNumber = 4;
   }
 }
 
@@ -217,10 +233,6 @@ function render2() {
   ctx.drawImage(x, 0, 0);
 }
 
-function testCanvas() {
-  ctx.fillRect(300, 300, 100, 100);
-}
-
 function findRoom(list, room) {
   let check = true;
   for(let index = 0; index < list.length; ++index) { // right now assumes all rooms are rectangles, got to fix that. 
@@ -234,28 +246,42 @@ function findRoom(list, room) {
   }
 }
 
-function drawRectangle(room) { // might want to redraw the image or have some way of clearing the rectangle. 
-ctx.fillRect(room.cords[0][0], room.cords[0][1], room.cords[1][0]-room.cords[0][0], room.cords[1][1]-room.cords[0][1]);
+function drawRectangle(room) { 
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ctx.stroke();
+  render2(); // Redraws the image so we don't highlight multiple rooms.
+  ctx.beginPath();
+  ctx.lineWidth = "6";
+  ctx.strokeStyle = "yellow";
+  ctx.rect(room.cords[0][0], room.cords[0][1], room.cords[1][0]-room.cords[0][0], room.cords[1][1]-room.cords[0][1]);
+  ctx.stroke();
+  // does not work for vaules over 400 not sure why?
+
 }
 
 function begin() { // need to have a way to change the floor of the building based on the cordinate or have some sort of check for that 
   let building = document.getElementById("build").value;
   let room = document.getElementById("textbox").value;
-  if (building == "carn") {
-    findRoom(carnegie, room);
-  }
-  if (building == "library") {
-    alert("We have not implemented this building yet");
+  if(room.charAt(0) == floorNumber) {
+    if (building == "carn") {
+      findRoom(carnegie, room);
+    }
+    if (building == "library") {
+      alert("We have not implemented this building yet");
 
-  }
-  if (building == "hum") {
-    alert("We have not implemented this building yet");
+    }
+    if (building == "hum") {
+      alert("We have not implemented this building yet");
 
-  }
-  if (building == "orl") {
-    alert("We have not implemented this building yet");
+    }
+    if (building == "orl") {
+      alert("We have not implemented this building yet");
 
-  }
+    }
+}
+else {
+  alert("Room is not on this floor");
+}
 
 
 }
